4c1a3b86882cb1905d18e262f6b5532dd4f68ac5
Alessandro <alesan.t90@gmail.com>
2019-05-25 02:10:30 +0200

    fix styles and import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { NgSiemaModule } from 'ng-siema';
+import { NgSiemaModule } from 'projects/ng-siema/src/public-api';
 
 @NgModule({
   declarations: [
